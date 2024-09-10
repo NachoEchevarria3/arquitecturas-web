e7f3d99b1d0ddccf465e8da8dd7745fda8905a2d
Ale Guay <galeguay@gmail.com>
2024-09-10 16:42:21 -0300

    Se completo implementacion de Factura Producto

--- EjercicioIntegrador1/src/main/java/com/jie/dao/FacturaProductoDaoImpl.java
+++ EjercicioIntegrador1/src/main/java/com/jie/dao/FacturaProductoDaoImpl.java
@@ -3,6 +3,8 @@ package com.jie.dao;
 import com.jie.factory.Factory;
 import com.jie.model.FacturaProducto;
 import com.jie.util.HelperMySql;
+import com.jie.service.ServicioFacturas;
+import com.jie.service.ServicioProductos;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -48,21 +50,62 @@ public class FacturaProductoDaoImpl implements Dao<FacturaProducto> {
 
     @Override
     public void update(FacturaProducto facturaProducto) throws SQLException {
+        String query = "UPDATE factura_producto SET cantidad = ? WHERE id_factura = ? AND id_producto = ?";
 
+        try (Connection conn = factory.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(query)) {
+            pstmt.setInt(1, facturaProducto.getCantidad());
+            pstmt.setInt(2, facturaProducto.getFactura().getId());
+            pstmt.setInt(3, facturaProducto.getProducto().getId());
+            pstmt.executeUpdate();
+        }
     }
 
     @Override
-    public void delete(FacturaProducto facturaProducto) {
+    public void delete(FacturaProducto facturaProducto) throws SQLException {
+        String query = "DELETE FROM factura_producto WHERE id_factura = ? AND id_producto = ?";
 
+        try (Connection conn = factory.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(query)) {
+            pstmt.setInt(1, facturaProducto.getFactura().getId());
+            pstmt.setInt(2, facturaProducto.getProducto().getId());
+            pstmt.executeUpdate();
+        }
     }
 
     @Override
-    public FacturaProducto get(int id) {
-        return null;
+    public FacturaProducto get(int idFactura, int idProducto) throws SQLException {
+        FacturaProducto facturaProducto = null;
+        String query = "SELECT * FROM factura_producto WHERE id_factura = ? AND id_producto = ?";
+        ServicioFacturas servicioFacturas = new ServicioFacturas();
+        ServicioProductos servicioProducto = new ServicioProductos();
+
+        try (Connection conn = factory.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(query)) {
+            pstmt.setInt(1, idFactura);
+            pstmt.setInt(2, idProducto);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                facturaProducto = new FacturaProducto(servicioFacturas.get(rs.getInt("id_factura")), servicioProductos(rs.getInt("id_producto")), rs.getInt("cantidad"));
+            }
+        }
+
+        return facturaProducto;
     }
 
     @Override
-    public List<FacturaProducto> getAll() {
-        return List.of();
+    public List<FacturaProducto> getAll() throws SQLException {
+        List<FacturaProducto> facturaProducto = new ArrayList<>();
+
+        String query = "SELECT * FROM factura_producto";
+        try (Connection conn = factory.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(query)) {
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                facturaProducto.add(new FacturaProducto(servicioFacturas.get(rs.getInt("id_factura")), servicioProductos(rs.getInt("id_producto")), rs.getInt("cantidad")));
+            }
+        }
+
+        return facturaProducto;
     }
 }
--- EjercicioIntegrador1/src/main/java/com/jie/service/ServicioFacturasProductos.java
+++ EjercicioIntegrador1/src/main/java/com/jie/service/ServicioFacturasProductos.java
@@ -21,4 +21,39 @@ public class ServicioFacturasProductos {
             throw new RuntimeException(e);
         }
     }
+
+
+    public FacturasProductos findById(int idFactura, int idProducto) {
+        try {
+            return this.daoFacturaProducto.get(int idFactura, int idProducto);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<FacturasProductos> findAll() {
+        try {
+            return this.daoFacturaProducto.getAll();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void delete(FacturasProductos facturaProducto) {
+        if (facturaProducto == null) throw new IllegalArgumentException("La factura no puede ser nulo");
+        try {
+            this.daoFacturaProducto.delete(facturaProducto);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void update(FacturasProductos facturaProducto) {
+        if (facturaProducto == null) throw new IllegalArgumentException("La factura no puede ser nula");
+        try {
+            this.daoFacturaProducto.update(facturaProducto);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
