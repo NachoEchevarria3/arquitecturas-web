5ebd94516f43194fc03b85283001633f98148afa
Ale Guay <galeguay@gmail.com>
2024-09-11 19:32:27 -0300

    Se modifico el Main

--- EjercicioIntegrador1/src/main/java/com/jie/Main.java
+++ EjercicioIntegrador1/src/main/java/com/jie/Main.java
@@ -1,14 +1,21 @@
 package com.jie;
 
+import com.jie.dao.ClienteDaoImpl;
+import com.jie.dao.FacturaDaoImpl;
+import com.jie.dao.FacturaProductoDaoImpl;
+import com.jie.dao.ProductoDaoImpl;
 import com.jie.factory.Factory;
 import com.jie.service.ServicioClientes;
 import com.jie.service.ServicioProductos;
 import com.jie.util.HelperCSV;
 
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Trabajo Práctico 1 - Arquitecturas Web");
         // HelperCSV helperCSV = new HelperCSV(Factory.getFactory(Factory.dbTypes.MYSQL));
+        cargarDB();
         ServicioClientes servicioClientes = new ServicioClientes(Factory.getFactory(Factory.dbTypes.MYSQL));
         servicioClientes.facturacionTotalClientes().forEach(System.out::println);
         ServicioProductos servicioProductos = new ServicioProductos(Factory.getFactory(Factory.dbTypes.MYSQL));
@@ -16,15 +23,15 @@ public class Main {
 
     }
 
-    private void prepopulateDB(){
-        ClienteDaoImpl clientes = new ClienteDaoImpl(Factory.getFactory(Factory.dbTypes.MYSQL));
-        clientes.createTable();
-        ProductoDaoImpl pd = new ProductoDaoImpl(Factory.getFactory(Factory.dbTypes.MYSQL));
-        pd.createTable();
-        FacturaDaoImpl ffdao = new FacturaDaoImpl(Factory.getFactory(Factory.dbTypes.MYSQL));
-        ffdao.createTable();
-        FacturaProductoDaoImpl facturas = new FacturaProductoDaoImpl(Factory.getFactory(Factory.dbTypes.MYSQL));
-        facturas.createTable();
+    private static void cargarDB(){
+        try {
+        new ClienteDaoImpl(Factory.getFactory(Factory.dbTypes.MYSQL)).createTable();
+        new ProductoDaoImpl(Factory.getFactory(Factory.dbTypes.MYSQL)).createTable();
+        new FacturaDaoImpl(Factory.getFactory(Factory.dbTypes.MYSQL)).createTable();
+        new FacturaProductoDaoImpl(Factory.getFactory(Factory.dbTypes.MYSQL)).createTable();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
         HelperCSV helperCSV = new HelperCSV(Factory.getFactory(Factory.dbTypes.MYSQL));
         helperCSV.readCsv("clientes.csv");
